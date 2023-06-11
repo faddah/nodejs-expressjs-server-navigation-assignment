e431312f8179d4ad05b7ce5705b4eaa963b7fe9b
Faddah Wolf <my_biz@me.com>
2023-06-10 21:42:10 -0700

    Edited app.js to have vars for server console.log() string.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log(`Node.JS with Express.JS server now running @ http://127.0.0.1:3000`));
+const protocol = `http`;
+const server = `127.0.0.1`
+const port = `3000`
+
+app.listen(3000, () => console.log(`Node.JS with Express.JS server now running @ ${protocol}://${server}:${port}.`));
