ddcbd983fac0b14181f200325e78025c83718832
Faddah Wolf <my_biz@me.com>
2023-07-07 14:51:41 -0700

    Removed commented-out code.

--- app.js
+++ app.js
@@ -13,24 +13,6 @@ const coRoutes = require('./routes/company');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* 
-app.use('/users', (req, res, next) => {
-	console.log('In the Second Users Page Middleware!');
-	res.send(`
-		<h1>The Users Page</h1>
-		<a href="/" alt="Link to The Company Home Page"><h3>Go Back To The Company Home Page</h3></a>.
-	`);
-});
-
-app.use('/', (req, res, next) => {
-	console.log('In the Main Company Home Page Middleware!');
-	res.send(`
-		<h1>Welcome to Our Company Home Page Node.JS & Express App!</h1>
-		<a href="/users" alt"Link to Our Users Page"><h3>Go To Our Users Page</h3></a>.
-	`);
-});
- */
-
 app.use(usersRoutes);
 app.use(coRoutes);
 
